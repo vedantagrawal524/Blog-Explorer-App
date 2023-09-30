9a801c2f1ec9398c157aa28a576e121d6c71609f
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-10-01 00:21:17 +0530

    Api itegrated

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,6 @@
 import 'package:blogexplorer/screens/blogs_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -10,7 +11,37 @@ class TabsScreen extends StatefulWidget {
   }
 }
 
+void fetchBlogs() async {
+  const String url = 'https://intent-kit-16.hasura.app/api/rest/blogs';
+  const String adminSecret =
+      '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6';
+
+  try {
+    final response = await http.get(Uri.parse(url), headers: {
+      'x-hasura-admin-secret': adminSecret,
+    });
+
+    if (response.statusCode == 200) {
+      // Request successful, handle the response data here
+      print('Response data: ${response.body}');
+    } else {
+      // Request failed
+      print('Request failed with status code: ${response.statusCode}');
+      print('Response data: ${response.body}');
+    }
+  } catch (e) {
+    // Handle any errors that occurred during the request
+    print('Error: $e');
+  }
+}
+
 class _TabsScreenState extends State<TabsScreen> {
+  @override
+  void initState() {
+    fetchBlogs();
+    super.initState();
+  }
+
   int _selectedScreenIndex = 0;
   void _selectScreen(int index) {
     setState(() {
